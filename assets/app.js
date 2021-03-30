1f973d4e992f8e815c6821338ab4647ae378aa3a
Josh <3430+joshutt@users.noreply.github.com>
2021-03-30 15:13:35 -0400

    Basic layout

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,19 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+// import './styles/bootstrap.min.css';
 import './styles/app.scss';
 
 // start the Stimulus application
 // import './bootstrap';
 // JS file needed on every page
-import './core';
+// import './core';
+
+// require bootstrap.js
+const $ = require('jquery');
+require('bootstrap');
+
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
+
